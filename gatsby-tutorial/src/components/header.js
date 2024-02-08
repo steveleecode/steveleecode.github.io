e77d5af9a44cb30191493d2960479a0470351f37
Stephen <stevelee_2008@outlook.com>
2024-02-07 22:42:33 -0800

    fixed issue with git

---
+++ gatsby-tutorial/src/components/header.js
@@ -0,0 +1,14 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const Header = () => {
+  return (
+    <main style={{ textAlign: "center", outline: "solid black", background: "black", borderRadius: "20px", color: "white", fontFamily: "monospace", fontSize: "2em" }}>
+        <h1>Steve Lee</h1> 
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default Header
