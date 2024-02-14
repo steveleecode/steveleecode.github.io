e1b6bc3c96a1493a96ac236be80adcad4c5188f1
steveleecode <stevelee_2008@outlook.com>
2024-02-14 14:29:37 -0800

    Fixed GHPages Push and Added Open In New Tab

--- gatsby-tutorial/src/pages/repos.js
+++ gatsby-tutorial/src/pages/repos.js
@@ -7,16 +7,16 @@ const RepoPage = () => {
     <Layout>
         <div class="RepoPage">
             <div className={repos.post}>
-                <a href="https://github.com/steveleecode">
+                <a href="https://github.com/steveleecode" target="_blank">
                     <img alt="steveleecode" src="https://github-readme-stats.vercel.app/api/?username=steveleecode&amp;theme=dark&amp;show_icons=true&amp;card_width=945px"></img>
                 </a>
                 <hr></hr>
                 <h1 className={repos.repoHeader}>Repositories</h1>
                 <div className={repos.repositories}>
-                    <a className = {repos.repository}href="https://github.com/steveleecode/steveleecode.github.io">
+                    <a className = {repos.repository}href="https://github.com/steveleecode/steveleecode.github.io"target="_blank">
                         <img alt="steveleecode/steveleecode.github.io" src="https://github-readme-stats.vercel.app/api/pin/?username=steveleecode&amp;repo=steveleecode.github.io&amp;theme=dark&amp;show_owner=false&amp;card_width=9px"></img>          
                     </a>
-                    <a className = {repos.repository} href="https://github.com/steveleecode/TOFSensor">
+                    <a className = {repos.repository} href="https://github.com/steveleecode/TOFSensor"target="_blank">
                         <img alt="steveleecode/TOFSensor" src="https://github-readme-stats.vercel.app/api/pin/?username=steveleecode&amp;repo=TOFSensor&amp;theme=dark&amp;show_owner=false&amp;card_width=900px"></img>          
                     </a>
                 </div>
